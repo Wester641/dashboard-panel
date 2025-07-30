b628d9d843efba76bbabbe408ae3e0cfb8201a69
zafarzhon <zafarzhon77@gmail.com>
2025-07-30 18:37:56 +0600

    [added] MediaCard and delete function them

---
+++ src/components/MediaCard/MediaCard.tsx
@@ -0,0 +1,92 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import type { ProductGET } from "../../types/FormTypes";
+import styles from "./MediaCard.module.scss";
+import { axiosInstance } from "../../lib/axios";
+
+type MediaCardProps = {
+  products: ProductGET;
+};
+
+// id: number;
+// stock_state: "Available" | "OutOfStock" | "PreOrder";
+// total_stock: number;
+// rating: number | string;
+// reviewCount: number | string;
+// title: string;
+// shop_name: string;
+// price: number;
+// old_price: string;
+// new_price: string;
+// image: string;
+// delivered_by: string;
+// discount: string;
+// sku: string;
+// description: string;
+// specifications: {
+//   spec_images: string[];
+// };
+// colors: string[];
+// tags: string[];
+
+export default function MediaCard({ products }: MediaCardProps) {
+  const handleDelete = async () => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this product?"
+    );
+    if (confirm) {
+      const response = await axiosInstance.delete("/products/" + products.id);
+      console.log(response);
+    }
+
+    if (!confirm) return;
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <div className={styles.mediaCard}>
+        <img
+          className={styles.mediaCardImg}
+          src={
+            products.specifications?.spec_images?.[0] ||
+            "https://res.cloudinary.com/dx2cycu19/image/upload/v1747590123/samples/chair.png"
+          }
+          alt="img"
+        />
+      </div>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {products.title}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {products.description}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {products.id}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {products.stock_state}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {products.total_stock}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant="contained"
+          color="error"
+          size="small"
+          onClick={handleDelete}
+        >
+          Delete
+        </Button>
+        <Button variant="contained" color="primary" size="small">
+          Edit
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/components/ProductsCard/ProductsCard.tsx
+++ src/components/ProductsCard/ProductsCard.tsx
@@ -1,6 +1,6 @@
-import { Box, LinearProgress } from "@mui/material";
+import { Box, Container, Grid, LinearProgress } from "@mui/material";
 import type { ProductGET } from "../../types/FormTypes";
-import styles from "./ProductsCard.module.scss";
+import MediaCard from "../MediaCard/MediaCard";
 
 type ProductsProps = {
   error: any;
@@ -26,63 +26,16 @@ function ProductsCard({ products, error, loading }: ProductsProps) {
   }
 
   return (
-    <div>
+    <Container>
       <h1>Products</h1>
-
-      {products.map((product: ProductGET) => (
-        <div className={styles.productCard} key={product.id}>
-          <p>
-            <strong>name:</strong> {product.title}
-          </p>
-          <p>
-            <strong>description:</strong> {product.description}
-          </p>
-          <p>
-            <strong>price:</strong> {product.price}
-          </p>
-          <p>
-            <strong>new_price:</strong> {product.new_price}
-          </p>
-          <p>
-            <strong>old_price:</strong> {product.old_price}
-          </p>
-          <p>
-            <strong>rating:</strong> {product.rating}
-          </p>
-          <p>
-            <strong>reviewCount:</strong> {product.reviewCount}
-          </p>
-          <p>
-            <strong>shop_name:</strong> {product.shop_name}
-          </p>
-          <p>
-            <strong>sku:</strong> {product.sku}
-          </p>
-          <p>
-            <strong>stock_state:</strong> {product.stock_state}
-          </p>
-          <p>
-            <strong>total_stock:</strong> {product.total_stock}
-          </p>
-          <p>
-            <strong>discount:</strong> {product.discount}
-          </p>
-          <p>
-            <strong>delivered_by:</strong> {product.delivered_by}
-          </p>
-          <p>
-            <strong>colors:</strong> {product.colors?.[0] || "No colors"}
-          </p>
-          <p>
-            <strong>tags:</strong> {product.tags?.[0] || "No tags"}
-          </p>
-          <p>
-            <strong>specifications:</strong>{" "}
-            {product.specifications?.spec_images?.[0] || "No specifications"}
-          </p>
-        </div>
-      ))}
-    </div>
+      <Grid container spacing={2}>
+        {products.map((product: ProductGET) => (
+          <Grid size={{ xs: 12, md: 3 }} key={product.id}>
+            <MediaCard products={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
 
