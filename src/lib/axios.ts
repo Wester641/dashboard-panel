83d2e4be89cb5e9469c705963629e23396fecd90
zafarzhon <zafarzhon77@gmail.com>
2025-09-29 20:59:26 +0600

    [added env]

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://165.227.142.6/api/v1/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
